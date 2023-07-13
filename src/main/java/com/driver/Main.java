e06c275da1907c86a5a1777d24beab9c57b5aa50
Pankaj-Shinde-19 <pankajharishinde19@gmail.com>
2023-07-13 17:20:45 +0530

    Code changes Done 1st time

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
+    public static class Product {
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
 
+    public static void main(String args[]) {
+        Product p = new Product();
+
+        int result0 = p.product(2,4);
+        System.out.println("result0 :- "+result0);
+
+        int result1 = p.product(2,4, 5);
+        System.out.println("result1 :- "+result1);
+
+        double result2 = p.product(2.56,4.987);
+        System.out.println("result2 :- "+result2);
+    }
 }
